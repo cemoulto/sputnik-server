ff4025b33462e36f634d8dad322b62961c7e88c5
Henning Peters <pete@dexterslab.de>
2015-10-28 16:28:10 +0100

    fix debug mode

--- application.py
+++ application.py
@@ -1,5 +1,9 @@
+import os
 from shuttle_server import app as application
 
 
+application.debug = os.environ.get('DEBUG') == 'True'
+
+
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run()
